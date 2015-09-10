2531a90b838b4d44d1e620ebfd202a1d19969478
Oliver Roick <oliver.roick@gmail.com>
2015-09-10 12:40:11 +0100

    Add travis

---
+++ travis/settings/settings.py
@@ -0,0 +1,34 @@
+import os.path
+from geokey.core.settings.dev import *
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+DEFAULT_FROM_EMAIL = 'sender@example.com'
+ACCOUNT_EMAIL_VERIFICATION = 'optional'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'geokey',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+INSTALLED_APPS += (
+    'geokey_export',
+)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = normpath(join(dirname(dirname(abspath(__file__))), 'assets'))
+MEDIA_URL = '/assets/'
+
+WSGI_APPLICATION = 'settings.wsgi.application'
+
+ROOT_URLCONF = 'settings.urls'
