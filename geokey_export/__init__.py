b348ea3c57ca98273c4c6e650e87e7a6c789603e
Julius O <j.osokinas@mappingforchange.org.uk>
2015-11-20 14:59:57 +0000

    Improved setup

--- geokey_export/__init__.py
+++ geokey_export/__init__.py
@@ -1,6 +1,9 @@
 from geokey.extensions.base import register
 
 
+VERSION = (0, 1, 0)
+__version__ = '.'.join(map(str, VERSION))
+
 register(
     'geokey_export',
     'Export',
--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 
-import os
-del os.link
-
+from os.path import join
 from setuptools import setup, find_packages
 
 
+name = 'geokey-export'
+version = __import__(name.replace('-', '_')).__version__
+repository = join('https://github.com/ExCiteS', name)
+
 setup(
-    name='geokey-export',
-    version='0.1.0',
+    name=name,
+    version=version,
+    description='Export data from GeoKey to KML or GeoJSON',
+    url=repository,
+    download_url=join(repository, 'tarball', version),
     author='Patrick Rickles',
     author_email='p.rickles@ucl.ac.uk',
+    license='MIT',
     packages=find_packages(exclude=['*.tests', '*.tests.*', 'tests.*']),
     include_package_data=True,
 )
