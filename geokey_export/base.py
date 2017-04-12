a9fbb73fc9182353c8e7889a49e66d5364245a5a
Carles Boils Gisbert <c.gisbert@ucl.ac.uk>
2017-04-12 15:20:45 +0100

    change name for observation keys

--- geokey_export/base.py
+++ geokey_export/base.py
@@ -6,4 +6,4 @@ media_keys = ['file_id', 'file_type', 'contribution_id', 'creator',
 comment_keys = ['comment_id', 'contribution_id', 'creator', 'creator_id',
 'created_at', 'respondsto', 'text']
 
-keys = ['geom', 'id', 'creator', 'creator_id', 'created_at', 'status']
+keys_obs = ['geom', 'id', 'creator', 'creator_id', 'created_at', 'status']
